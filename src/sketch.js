0cadbda8abf9083d103dbc1cd0d771f6172fa48c
coderdidit <86926500+coderdidit@users.noreply.github.com>
2021-09-03 22:59:36 +0100

    Preview svg wiht Moralis Cloud Function (#1)
    * load svg from base64 encoded string
    
    * add getSVG moralis cloud fn
    
    * display previewed aavegotchi
    
    * be able to preview aavegitchi from the smart contract
    
    * remove bg
    
    Co-authored-by: mkuzdowicz <martin.kuzdowicz@gmail.com>

--- src/sketch.js
+++ src/sketch.js
@@ -1,85 +1,117 @@
 import party from "party-js"
-import svgPath from "./vendor/icon/aavegotchi.svg"
+import * as MoralisSDK from 'moralis'
 
-const canvasParent = document.getElementById('main-canvas')
+// init moralis
+const moralisAppID = process.env.MORALIS_APPLICATION_ID || ''
+const moralisServerUrl = process.env.MORALIS_SERVER_URL || ''
+const Moralis = MoralisSDK.default
+Moralis.initialize(moralisAppID)
+Moralis.serverURL = moralisServerUrl
 
-let img;
-const gotchiSize = 80
+// fetch and setup player SVG
+const setupPlayerSVG = async () => {
+  const numericTraits = [1, 5, 99, 29, 1, 1]
+  const equippedWearables = [23, 8, 4, 43, 0, 4, 0, 1, 0, 0, 0, 3, 7, 0, 0, 0]
+  const rawSVG = await Moralis.Cloud.run("getSVG", { numericTraits: numericTraits, equippedWearables: equippedWearables })
+  const removeBG = (svg) => {
+    const styledSvg = svg.replace("<style>", "<style>.gotchi-bg,.wearable-bg{display: none}");
+    return styledSvg;
+  };
+  const rawSVGNoBG = removeBG(rawSVG)
+  const svgStrBase64 = window.btoa(rawSVGNoBG)
+  const svgDataUri = `data:image/svg+xml;base64,${svgStrBase64}`
 
-const loadImgFn = () => {
-  img = window.loadImage(svgPath)
-  console.log('image loaded', img)
+  const aavegotchiPreview = document.getElementById('aavegotchi-preview')
+  aavegotchiPreview.src = svgDataUri
+  return svgDataUri
 }
 
-window.preload = () => {
-  loadImgFn()
-}
+const setupSketck = async () => {
+  const svgDataUri = await setupPlayerSVG()
+  const canvasParent = document.getElementById('main-canvas')
 
-class Ball {
-  constructor(x, y) {
-    this.x = x
-    this.y = y - 50
-    this.speed = 1.8
+  let img;
+  const gotchiSize = 80
+
+  const loadImgFn = () => {
+    img = window.loadImage(svgDataUri)
+    console.log('image loaded', img)
   }
 
-  draw() {
-    image(img, this.x, this.y, gotchiSize, gotchiSize)
+  window.preload = () => {
+    loadImgFn()
   }
 
-  moveUp() {
-    // Moving up at a constant speed
-    this.y -= this.speed
-    // Reset to the bottom
-    // if top was reached
-    if (this.y < 0) {
-      // trigger confetti
-      party.confetti(canvasParent)
-      // reset to beginning
-      this.y = getHeight();
+  class Ball {
+    constructor(x, y) {
+      this.x = x
+      this.y = y - 50
+      this.speed = 1.8
+    }
+
+    draw() {
+      image(img, this.x, this.y, gotchiSize, gotchiSize)
+    }
+
+    moveUp() {
+      // Moving up at a constant speed
+      this.y -= this.speed
+      // Reset to the bottom
+      // if top was reached
+      if (this.y < 0) {
+        // trigger confetti
+        party.confetti(canvasParent)
+        // reset to beginning
+        this.y = getHeight() - 50;
+      }
+    }
+  }
+
+  function getHeight() {
+    return height - 25
+  }
+
+  function drawLadder() {
+    fill(81, 1, 176);
+    const rectWidth = 80
+    rect((window.innerWidth / 2) - rectWidth / 2, 0, 80, window.innerHeight - 90);
+  }
+
+  function drawBackground() {
+    noStroke();
+    background(18, 2, 47);
+  }
+
+  let ball
+
+  // needs to be defined in window for bundler
+  window.setup = () => {
+    console.log('setup p5js sketch')
+    var sketchCanvas = createCanvas(window.innerWidth, window.innerHeight - 90);
+    sketchCanvas.parent("main-canvas");
+
+    // init ball
+    let x = (window.innerWidth / 2) - (gotchiSize / 2);
+    let y = getHeight() - 9;
+    ball = new Ball(x, y)
+  }
+
+  // needs to be defined in window for bundler
+  window.draw = () => {
+    drawBackground()
+
+    drawLadder()
+
+    if (window.gameState) {
+      ball.draw()
+
+      if (window.gameStateIsInMove()) {
+        ball.moveUp()
+      }
     }
   }
 }
 
-function getHeight() {
-  return height - 25
-}
+// init sketch
+setupSketck()
 
-function drawLadder() {
-  fill(81, 1, 176);
-  const rectWidth = 80
-  rect((window.innerWidth / 2) - rectWidth / 2, 0, 80, window.innerHeight - 90);
-}
-
-function drawBackground() {
-  noStroke();
-  background(18, 2, 47);
-}
-
-let ball
-
-// needs to be defined in window for bundler
-window.setup = () => {
-  console.log('setup p5js sketch')
-  var sketchCanvas = createCanvas(window.innerWidth, window.innerHeight - 90);
-  sketchCanvas.parent("main-canvas");
-
-  // init ball
-  let x = (window.innerWidth / 2) - (gotchiSize / 2);
-  let y = getHeight() - 9;
-  ball = new Ball(x, y)
-}
-
-// needs to be defined in window for bundler
-window.draw = () => {
-  drawBackground()
-
-  drawLadder()
-
-  if (window.gameState) {
-    ball.draw()
-
-    if (window.gameStateIsInMove()) {
-      ball.moveUp()
-    }
-  }
-}
