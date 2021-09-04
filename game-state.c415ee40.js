f790934bd68c9b788b6c539cfeb6d562ab753bf8
mkuzdowicz <martin.kuzdowicz@gmail.com>
2021-09-04 13:16:46 +0100

    preview gotchi

--- game-state.c415ee40.js
+++ game-state.c415ee40.js
@@ -167,7 +167,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56752" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58156" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
