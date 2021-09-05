6ff9dcbf4dda7a2d47fdec68cf699c3cd735317b
mkuzdowicz <martin.kuzdowicz@gmail.com>
2021-09-05 22:55:46 +0100

    styling

--- bootstrap.min.5e83b927.js
+++ bootstrap.min.5e83b927.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62321" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62367" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -394,4 +394,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=https://coderdidit.com/aavegotchi-strech/bootstrap.min.5e83b927.js.map
+//# sourceMappingURL=/bootstrap.min.5e83b927.js.map
