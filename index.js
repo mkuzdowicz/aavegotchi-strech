eea6804aa549b594aa8f837194e8607691832993
mkuzdowicz <martin.kuzdowicz@gmail.com>
2021-09-07 11:50:01 +0100

    styling

--- index.js
+++ index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62528" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61080" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
